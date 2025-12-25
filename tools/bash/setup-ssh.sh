b22d74f66b3c2b4c85b6a282fc018325a422694c
kbntx <kenny.talbi@outlook.fr>
2025-12-25 14:21:44 +0000

    Try to deploy vault

--- tools/bash/setup-ssh.sh
+++ tools/bash/setup-ssh.sh
@@ -4,7 +4,7 @@ SSH_KEY="$1"
 KNOWN_HOSTS="$2"
 
 mkdir -p ~/.ssh
-echo -n "$SSH_KEY" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
+echo -n "$SSH_KEY" > ~/.ssh/id_ed25519
+chmod 600 ~/.ssh/id_ed25519
 echo -n "$KNOWN_HOSTS" > ~/.ssh/known_hosts
 chmod 600 ~/.ssh/known_hosts
